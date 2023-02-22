1ba2ed2f985f04fb2d76c05ae00d5f1c280f5c45
kiran <pskiran90@gmail.com>
2023-02-22 17:50:23 +0530

    completed ui

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import rive_common
+import smart_auth
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
+}
